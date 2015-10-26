c421a9844e7d9ae23e57f1947c824f2a4a830e9b
Dizzy <ches@ukr.net>
2015-10-26 17:57:19 +0200

    Added beforeSave() method to make sure that only POST requests are sent to this controller

--- protected/controllers/ReminderController.php
+++ protected/controllers/ReminderController.php
@@ -2,5 +2,16 @@
 
 class ReminderController extends CController
 {
+    /**
+     * BeforeAction, verifies that the request in an Ajaxy one.
+     * @param  CAction $action The Action
+     * @see CController::beforeAction($action)
+     */
+    public function beforeAction($action)
+    {
+        if (!Yii::app()->request->isPostRequest)
+            throw new CHttpException(400, 'Bad Request');
 
+        return parent::beforeAction($action);
+    }
 }
