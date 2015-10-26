20516da93b05f6a076f690e7b02cb3d0f69fc673
Dizzy <ches@ukr.net>
2015-10-26 18:10:16 +0200

    Added a save and delete methods

--- protected/controllers/ReminderController.php
+++ protected/controllers/ReminderController.php
@@ -16,6 +16,52 @@ class ReminderController extends CController
     }
 
     /**
+     * Handles updating of Reminders
+     * @param  int $id The model ID
+     */
+    public function actionSave($id = NULL)
+    {
+        if ($id != NULL)
+            $model = $this->loadModel($id);
+        else
+            $model = new Reminders;
+
+        if (isset($_POST['Reminders']))
+        {
+            $model->attributes = $_POST['Reminders'];
+
+            // Prevent modification of the model if the user does not own this event
+            if (!$this->loadEvent($model->event_id))
+                return false;
+
+            if ($model->save())
+                return true;
+            else
+                throw new CHttpException(400, print_r($model->getErrors(), true));
+        }
+
+        return true;
+    }
+
+    /**
+     * Deletes an event
+     * @param  int     $id    The Model ID
+     */
+    public function actionDelete($id = NULL)
+    {
+        $model = $this->loadModel($id);
+
+        // Prevent modification of the model if the user does not own this event
+        if (!$this->loadEvent($model->event_id))
+            return false;
+
+        if ($model->delete())
+            return true;
+
+        throw new CHttpException(400, 'Bad Request');
+    }
+
+    /**
      * Loads an Event model associated to this reminder, and verifies that the current user has access to it
      * @param  int $event_id     The Event Id
      * @return boolean
