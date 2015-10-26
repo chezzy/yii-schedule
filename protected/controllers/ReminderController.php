f4c026c2c0db4a2a6ad65ebc186d70c3089d0e84
Dizzy <ches@ukr.net>
2015-10-26 18:07:12 +0200

    Added a method to load a particular reminder and another method to verify that we have access to the associated event for a particular reminder

--- protected/controllers/ReminderController.php
+++ protected/controllers/ReminderController.php
@@ -14,4 +14,39 @@ class ReminderController extends CController
 
         return parent::beforeAction($action);
     }
+
+    /**
+     * Loads an Event model associated to this reminder, and verifies that the current user has access to it
+     * @param  int $event_id     The Event Id
+     * @return boolean
+     */
+    private function loadEvent($event_id)
+    {
+        $event = Events::model()->findByPk($event_id);
+        if ($event == NULL)
+            return false;
+
+        if ($event->user_id != Yii::app()->user->id)
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Returns a model, or throws an exception if it is not found
+     * @param  int            $id    The ID of the model to find
+     * @return Events::model
+     */
+    private function loadModel($id)
+    {
+        if ($id == NULL)
+            throw new CHttpException(400, 'Bad Request');
+
+        $model = Reminders::model()->findByPk($id);
+
+        if ($model == NULL)
+            throw new CHttpException(404, 'No model with that ID was found');
+
+        return $model;
+    }
 }
